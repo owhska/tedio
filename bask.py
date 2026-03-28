514805d7a4d14d36ce9e0bc4306f4bb210ce18e8
PC <pc@owhska.com>
2026-03-28 16:59:57 -0300

    soma de sumeros com elixir

--- bask.py
+++ bask.py
@@ -1,6 +1,5 @@
 import math
 
-
 a = int(input("a= "))
 b = int(input("b= "))
 c = int(input("c= "))
---
+++ teste.ex
@@ -0,0 +1,6 @@
+IO.puts("teste")
+v1 = IO.gets("> ") |> String.trim() |> String.to_integer()
+v2 = IO.gets("> ") |> String.trim() |> String.to_integer()
+
+IO.puts("Resultado = #{v1 + v2}")
+
