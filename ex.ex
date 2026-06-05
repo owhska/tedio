ca714e4479daa8a1c8fbc906bb2299545be6e2d4
M Brunes <brunesaraujo@gmail.com>
2026-06-05 08:37:42 -0300

    .

--- ex.ex
+++ ex.ex
@@ -3,3 +3,5 @@ v1 = IO.gets("> ") |> String.trim() |> String.to_integer()
 v2 = IO.gets("> ") |> String.trim() |> String.to_integer()
 
 IO.puts("Resultado = #{v1 + v2}")
+
+IO.puts(Integer.pow(2, 2))
